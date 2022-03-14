f09e9f4a2100cfda184127394f4e15700c0a29b8
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-03-15 00:54:18 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -18,7 +18,7 @@ ACTV_CALLS = []
 @authorized_users_only
 async def pause(_, message: Message):
     await callsmusic.pytgcalls.pause_stream(message.chat.id)
-    await message.reply_text("▶️ 𝐏𝐚𝐮𝐬𝐞 😔🥀")
+    await message.reply_text("<b>⏸️ 𝙿𝙰𝚄𝚂𝙴 ⏸️</b>")
 
 
 @Client.on_message(command(["resume"]) & other_filters)
@@ -26,7 +26,7 @@ async def pause(_, message: Message):
 @authorized_users_only
 async def resume(_, message: Message):
     await callsmusic.pytgcalls.resume_stream(message.chat.id)
-    await message.reply_text("⏸ 𝐑𝐞𝐬𝐮𝐦𝐞 ❤️")
+    await message.reply_text("<b>▶️ 𝚁𝙴𝚂𝚄𝙼𝙴 ▶️</b>")
 
 
 @Client.on_message(command(["end"]) & other_filters)
@@ -39,7 +39,7 @@ async def stop(_, message: Message):
         pass
 
     await callsmusic.pytgcalls.leave_group_call(message.chat.id)
-    await message.reply_text("❌ 𝐒𝐭𝐨𝐩 🛑 𝐒𝐭𝐫𝐞𝐚𝐦𝐢𝐧𝐠 ✨")
+    await message.reply_text("<b>🚫 𝚂𝚃𝙾𝙿 𝚂𝚃𝚁𝙴𝙰𝙼𝙸𝙽𝙶 🚫</b>")
 
 @Client.on_message(command(["skip"]) & other_filters)
 @errors
@@ -50,7 +50,7 @@ async def skip(_, message: Message):
     for x in callsmusic.pytgcalls.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("❗ 𝐍𝐨𝐭𝐡𝐢𝐧𝐠 😔  𝐈𝐬 𝐏𝐥𝐚𝐲𝐢𝐧𝐠 🎶 𝐓𝐨 𝐒𝐤𝐢𝐩 🥀")
+        await message.reply_text("<b>❗ 𝙽𝙾𝚃𝙷𝙸𝙽𝙶 𝙸𝚂 𝙿𝙻𝙰𝚈𝙸𝙽𝙶 𝚃𝙾 𝚂𝙺𝙸𝙿 ❗</b>")
     else:
         queues.task_done(chat_id)
         
@@ -65,4 +65,4 @@ async def skip(_, message: Message):
                     ),
                 ),
             )
-    await message.reply_text("➡️ 𝐒𝐤𝐢𝐩 💫 𝐓𝐡𝐞 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 ✨ 𝐒𝐨𝐧𝐠 🥀")
+    await message.reply_text("<b>⏩ 𝚂𝙺𝙸𝙿 𝙲𝚄𝚁𝚁𝙴𝙽𝚃 𝚂𝙾𝙽𝙶 ⏩</b>")

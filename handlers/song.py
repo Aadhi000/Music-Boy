099c825f1717ad418586a9984fcf697a82a20c3e
Aadhi <87893573+Aadhi000@users.noreply.github.com>
2022-03-17 19:54:49 +0530

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -16,7 +16,6 @@ from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from youtubesearchpython import SearchVideos
 from yt_dlp import YoutubeDL
-import youtube_dl
 import requests
 
 def time_to_seconds(time):
